1db9f12c2b05c04b1483b6c5e75128ce5d649587
KKKUBAKKK <kuba.kindracki@gmail.com>
2024-04-25 11:47:11 +0200

    Initial commit

---
+++ LAB10_AiSD2_2023/Lab10.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using ASD.Graphs;
+
+namespace ASD
+{
+    public class Lab10 : MarshalByRefObject
+    {
+
+        /// <param name="labyrinth">Graf reprezentujący labirynt</param>
+        /// <param name="startingTorches">Ilość pochodni z jaką startują bohaterowie</param>
+        /// <param name="roomTorches">Ilość pochodni w poszczególnych pokojach</param>
+        /// <param name="debt>">Ilość złota jaką bohaterowie muszą zebrać</param>
+        /// <param name="roomGold">Ilość złota w poszczególnych pokojach</param>
+        /// <returns>Informację czy istnieje droga przez labirynt oraz tablicę reprezentującą kolejne wierzchołki na drodze. W przypadku, gdy zwracany jest false, wartość tego pola powinna być null.</returns>
+        public (bool routeExists, int[] route) FindEscape(Graph labyrinth, int startingTorches, int[] roomTorches, int debt, int[] roomGold)
+        {
+            bool[] visited = new bool[labyrinth.VertexCount];
+            visited[0] = true;
+            (var routeExists, var route) =
+                FindEscapeRec(labyrinth, startingTorches + roomTorches[0], roomTorches, debt - roomGold[0], roomGold, visited, 0);
+            if (route == null)
+                return (false, null);
+
+            route.Reverse();
+            return (true, route.ToArray());
+        }
+
+        public (bool routeExists, List<int> route) FindEscapeRec(Graph labyrinth, int startingTorches, int[] roomTorches, int debt, int[] roomGold, 
+            bool[] visited, int room)
+        {
+            
+            if (room == labyrinth.VertexCount - 1)
+            {
+                if (debt <= 0)
+                    return (true, new List<int>(){room});
+
+                return (false, null);
+            }
+            else if (startingTorches <= 0)
+            {
+                return (false, null);
+            }
+
+            bool routeExists = false;
+            List<int> route = null;
+            foreach (var neighbor in labyrinth.OutNeighbors(room))
+            {
+                if (visited[neighbor])
+                    continue;
+
+                visited[neighbor] = true;
+                (routeExists, route) =
+                    FindEscapeRec(labyrinth, startingTorches + roomTorches[neighbor] - 1, roomTorches, debt - roomGold[neighbor], roomGold, visited, neighbor);
+                visited[neighbor] = false;
+                
+                if (routeExists)
+                    break;
+            }
+            if (routeExists)
+                route.Add(room);
+            
+            return (routeExists, route);
+        }
+
+        /// <param name="labyrinth">Graf reprezentujący labirynt</param>
+        /// <param name="startingTorches">Ilość pochodni z jaką startują bohaterowie</param>
+        /// <param name="roomTorches">Ilość pochodni w poszczególnych pokojach</param>
+        /// <param name="debt">Ilość złota jaką bohaterowie muszą zebrać</param>
+        /// <param name="roomGold">Ilość złota w poszczególnych pokojach</param>
+        /// <param name="dragonDelay">Opóźnienie z jakim wystartuje smok</param>
+        /// <returns>Informację czy istnieje droga przez labirynt oraz tablicę reprezentującą kolejne wierzchołki na drodze. W przypadku, gdy zwracany jest false, wartość tego pola powinna być null.</returns>
+
+        public (bool routeExists, int[] route) FindEscapeWithHeadstart(Graph labyrinth, int startingTorches, int[] roomTorches, int debt, int[] roomGold, int dragonDelay)
+        {
+            int[] visited = new int[labyrinth.VertexCount];
+            visited[0] = 1;
+            (var routeExists, var route) =
+                FindEscapeWithHeadstartRec(labyrinth, startingTorches + roomTorches[0], roomTorches, debt - roomGold[0], roomGold, dragonDelay + 1, visited, 0);
+            if (route == null)
+                return (false, null);
+
+            route.Reverse();
+            return (true, route.ToArray());
+            
+            return (false, null);
+        }
+        
+        public (bool routeExists, List<int> route) FindEscapeWithHeadstartRec(Graph labyrinth, int startingTorches, int[] roomTorches, int debt, int[] roomGold, 
+            int dragonDelay, int[] visited, int room)
+        {
+            
+            if (room == labyrinth.VertexCount - 1)
+            {
+                if (debt <= 0)
+                    return (true, new List<int>(){room});
+
+                return (false, null);
+            }
+            else if (startingTorches <= 0)
+            {
+                return (false, null);
+            }
+
+            bool routeExists = false;
+            List<int> route = null;
+            foreach (var neighbor in labyrinth.OutNeighbors(room))
+            {
+                if (visited[neighbor] != 0 && visited[room] + 1 - visited[neighbor] > dragonDelay)
+                    continue;
+
+                if (visited[neighbor] != 0)
+                {
+                    int oldDelay = dragonDelay;
+                    dragonDelay -= visited[room] + 1 - visited[neighbor];
+                    int temp = visited[neighbor];
+                    visited[neighbor] = visited[room] + 1;
+                    (routeExists, route) =
+                        FindEscapeWithHeadstartRec(labyrinth, startingTorches - 1, roomTorches,
+                            debt, roomGold, dragonDelay, visited, neighbor);
+                    dragonDelay = oldDelay;
+                    visited[neighbor] = temp;
+                }
+                else
+                {
+                    visited[neighbor] = visited[room] + 1;
+                    (routeExists, route) =
+                        FindEscapeWithHeadstartRec(labyrinth, startingTorches + roomTorches[neighbor] - 1, roomTorches,
+                            debt - roomGold[neighbor], roomGold, dragonDelay, visited, neighbor);
+                    visited[neighbor] = 0;
+                }
+                
+                if (routeExists)
+                    break;
+            }
+            if (routeExists)
+                route.Add(room);
+            
+            return (routeExists, route);
+        }
+    }
+}
